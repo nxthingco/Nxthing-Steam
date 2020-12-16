cfd18f287ce7e73d1d17216284137ea172329a49
nxthingco <76076144+nxthingco@users.noreply.github.com>
2020-12-16 03:57:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 __version__ = '1.05'
+
 import sys
 sys.path.insert(0,'D:/XSteam/1/lib')
 from SteamIdle import *
@@ -7,16 +8,17 @@ from ParseSteamProfile import *
 from SteamRetviver import *
 from RegisterKeys import *
 from GetSteamBackground import *
-from timerecord import *
 
 if __name__ == '__main__':
-	print(nSettings["RestartGames"])
 	print(Xtag + 'All loaded')
-	
-	if nSettings['RestartGames'] == True:
+	"""if nSettings['RestartGames'] == True:
 		print('Games will restart in 3 hours')
 		while True:
 			idle(nSettings['IdleMethod'])
 			time.sleep(nSettings["RestartTime"])
 	else:
-		idle(nSettings['IdleMethod'])
+		idle(nSettings['IdleMethod'])"""
+
+#GetSteamBackGround('https://steamcommunity.com/profiles/76561198451887896')
+#SteamRevive()
+#idle('SingleAccount')
