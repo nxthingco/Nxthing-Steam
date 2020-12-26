5b5ef221730621e29f398cd7a1f3a779a552e02c
nxthingco <76076144+nxthingco@users.noreply.github.com>
2020-12-26 20:58:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-__version__ = '1.05'
+__version__ = '1.06'
 
 import sys
-sys.path.insert(0,'D:/XSteam/1/lib')
+import os
+sys.path.insert(0,'D:/XSteam/Nxthing-Steam/lib')
 from SteamIdle import *
 from GetSteamIDS import get_steam_info as GSI
-from ParseSteamProfile import *
+from new_ParseSteamProfile import *
 from SteamRetviver import *
 from RegisterKeys import *
 from GetSteamBackground import *
+from SteamMachineInfo import *
+from ApiSteam import *
+from GUI import *
 
 if __name__ == '__main__':
 	print(Xtag + 'All loaded')
@@ -18,7 +22,7 @@ if __name__ == '__main__':
 			time.sleep(nSettings["RestartTime"])
 	else:
 		idle(nSettings['IdleMethod'])"""
-
+		
 #GetSteamBackGround('https://steamcommunity.com/profiles/76561198451887896')
 #SteamRevive()
 #idle('SingleAccount')
