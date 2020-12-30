08e94c3db2f8d31c4700b80b568c014027f8cc8c
nxthingco <76076144+nxthingco@users.noreply.github.com>
2020-12-30 18:24:09 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
-__version__ = '1.06'
+version = '1.11'
 
 import sys
-import os
-sys.path.insert(0,'D:/XSteam/Nxthing-Steam/lib')
+libpath = sys.path[0] + '\lib'
+sys.path.insert(0,libpath)
 from SteamIdle import *
-from GetSteamIDS import get_steam_info as GSI
+from GetSteamIDS import get_steam_info as gsi
 from new_ParseSteamProfile import *
-from SteamRetviver import *
 from RegisterKeys import *
-from GetSteamBackground import *
-from SteamMachineInfo import *
 from ApiSteam import *
-from GUI import *
+from update import *
+#from GUI import *
 
 if __name__ == '__main__':
-	print(Xtag + 'All loaded')
-	"""if nSettings['RestartGames'] == True:
-		print('Games will restart in 3 hours')
-		while True:
-			idle(nSettings['IdleMethod'])
-			time.sleep(nSettings["RestartTime"])
-	else:
-		idle(nSettings['IdleMethod'])"""
-		
-#GetSteamBackGround('https://steamcommunity.com/profiles/76561198451887896')
-#SteamRevive()
-#idle('SingleAccount')
+	print('All loaded')
+	CheckVersion(version)
