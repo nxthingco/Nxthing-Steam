84f0c0eda17e2880e732dc6c0e243199605fa08f
nxthingco <76076144+nxthingco@users.noreply.github.com>
2021-01-02 23:27:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-version = '1.11'
+version = '1.12'
 
 import sys
 libpath = sys.path[0] + '\lib'
 sys.path.insert(0,libpath)
 from SteamIdle import *
-from GetSteamIDS import get_steam_info as gsi
+from GetSteamIDS import get_steam_info as GSI
 from new_ParseSteamProfile import *
 from RegisterKeys import *
 from ApiSteam import *
 from update import *
-#from GUI import *
+from GUI import *
 
 if __name__ == '__main__':
 	print('All loaded')
 	CheckVersion(version)
+	OpenGui()
